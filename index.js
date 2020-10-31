b09aa723ca5c26e772c7b2a63d4c096bb6d3d4aa
June Kim <junexcix@gmail.com>
2020-10-31 13:56:39 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('BY GJK AND HJY')
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const title = 'by GJK and HJY';
+ 
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
+
+module.hot.accept();
