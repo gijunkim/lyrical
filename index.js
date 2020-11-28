a13eece7a6e09e31eff21f85b802acd8ce15bb2e
June Kim <junexcix@gmail.com>
2020-11-28 16:50:13 +0900

    Delete duplicate index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
 const express = require("express");
 const cookieParser =require("cookie-parser");
 const morgan = require("morgan");
